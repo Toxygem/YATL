a174f92880cd7bb0e53922d234ecc26768006926
khabib shahbanov <khabibtime@rambler.ru>
2026-03-12 13:36:01 +0300

    add create_request

--- src/yatl/step_executor.py
+++ src/yatl/step_executor.py
@@ -14,11 +14,15 @@ class StepExecutor:
         self.data_extractor = data_extractor
         self.template_renderer = template_renderer
 
-    def run_step(self, step: str, context: dict):
-        resolved_step = self.template_renderer.render_data(step, context)
+    def create_request(self, context, resolved_step):
         self.request_builder = RequestBuilder(context, resolved_step)
         data = self.request_builder.build()
         response = requests.request(**data)
+        return response
+
+    def run_step(self, step: str, context: dict):
+        resolved_step = self.template_renderer.render_data(step, context)
+        response = self.create_request(context, resolved_step)
 
         if "expect" in resolved_step:
             validator = ResponseValidator(response, resolved_step["expect"])
